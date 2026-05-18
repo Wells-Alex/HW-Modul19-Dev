92190d492f3e47f688bbcf26b1661a7cc38d3dd1
Wells-Alex <wells_@outlook.com>
2026-05-18 19:38:48 +0200

    Initial commit

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(5, 2 + 3);
+    }
+}
